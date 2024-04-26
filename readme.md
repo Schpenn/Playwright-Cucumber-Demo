37b1b5ce54d9f012ebf0d3f9a7892b04758a4c07
Espen Liland Bjørnsen <espen.bjornsen@bouvet.no>
2024-04-26 15:39:30 +0200

    Lagt til readme.md

---
+++ readme.md
@@ -0,0 +1,15 @@
+# Demo av Playwright med Cucumber
+
+## Det beste fra to verdner:
+Cucumber brukes til å beskrive tester (scenario) og steg med Gherkin-syntax, slik at det er lett å forstå hva som testes.
+
+Playwright brukes for å utføre handlingene i testene gjennom at stegene kobles til kode, slik at test-scenariene blir utført.
+
+```
+   **Given** I am on the home page
+   **When** I click on the button
+   **Then** something should happen
+``` 
+
+
+![alt text](image-1.png)
--- src/test/steps/playwrightExampleSteps.ts
+++ src/test/steps/playwrightExampleSteps.ts
@@ -3,9 +3,7 @@ import { expect } from '@playwright/test';
 import { pageFixture  } from "./hooks/pageFixture";
 
 Given('I navigate to the Website', async () =>{
-   // browser = await chromium.launch({headless:false});
-    //page = await browser.newPage();
-    await pageFixture.page.goto('https://playwright.dev/');
+  await pageFixture.page.goto('https://playwright.dev/');
   });
 
 Then('the website should have Playwright in the title', async () => {
@@ -13,9 +11,9 @@ Then('the website should have Playwright in the title', async () => {
 });
 
 When('I click on the link Get started', async () => {
-    await pageFixture.page.getByRole('link', { name: 'Get started' }).click();
+  await pageFixture.page.getByRole('link', { name: 'Get started' }).click();
 });
 
 Then('The heading Installation should be visible', async () =>{
-    await expect(pageFixture.page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  await expect(pageFixture.page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 })
